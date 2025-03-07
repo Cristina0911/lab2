1e2a58bdb7bccaee3cbace61863bc39022c3ee2f
Cristina <mariacristina.hategan@ulbsibiu.ro>
2025-03-07 09:31:03 +0200

    structuri clase

--- src/Triangle.java
+++ src/Triangle.java
@@ -6,6 +6,7 @@ public class Triangle extends Form{
         this.height = 0;
         this.base = 0;
     }
+
     public Triangle(String color,float height, float base){
         super(color);
         this.height = height;
